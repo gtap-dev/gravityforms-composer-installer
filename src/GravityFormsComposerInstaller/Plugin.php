f48d15380e44103659f355019a8e93139a2ab103
defunctl <justin.frydman@gmail.com>
2020-01-14 20:46:29 -0700

    Formatting

--- src/GravityFormsComposerInstaller/Plugin.php
+++ src/GravityFormsComposerInstaller/Plugin.php
@@ -15,78 +15,78 @@ class Plugin extends \FFraenz\PrivateComposerInstaller\Plugin implements PluginI
 
     const GRAVITY_FORMS_API = 'www.gravityhelp.com';
 
-	/**
-	 * Replaces placeholders with corresponding environment variables and overrides the download URL
-	 *
-	 * @param  PreFileDownloadEvent  $event
-	 *
-	 * @return void
-	 * @throws DownloadException
-	 * @throws \FFraenz\PrivateComposerInstaller\Exception\MissingEnvException
-	 */
-	public function injectPlaceholders(PreFileDownloadEvent $event): void
-	{
-		$url = $event->getProcessedUrl();
+    /**
+     * Replaces placeholders with corresponding environment variables and overrides the download URL
+     *
+     * @param  PreFileDownloadEvent  $event
+     *
+     * @return void
+     * @throws DownloadException
+     * @throws \FFraenz\PrivateComposerInstaller\Exception\MissingEnvException
+     */
+    public function injectPlaceholders(PreFileDownloadEvent $event): void
+    {
+        $url = $event->getProcessedUrl();
 
-		if (strpos($url, self::GRAVITY_FORMS_API) === false) {
-			return;
-		}
+        if (strpos($url, self::GRAVITY_FORMS_API) === false) {
+            return;
+        }
 
-		// Check if package url contains any placeholders
-		$placeholders = $this->getUrlPlaceholders($url);
+        // Check if package url contains any placeholders
+        $placeholders = $this->getUrlPlaceholders($url);
 
-		if (count($placeholders) > 0) {
-			// Replace each placeholder with env var
-			foreach ($placeholders as $placeholder) {
-				$value = $this->env->get($placeholder);
-				$url = str_replace('{%' . $placeholder . '}', $value, $url);
-			}
+        if (count($placeholders) > 0) {
+            // Replace each placeholder with env var
+            foreach ($placeholders as $placeholder) {
+                $value = $this->env->get($placeholder);
+                $url = str_replace('{%'.$placeholder.'}', $value, $url);
+            }
 
-			// Override the Gravity Forms' URL with the AWS URL retrieved from the API
-			$url = $this->getDownloadUrl($url);
+            // Override the Gravity Forms' URL with the AWS URL retrieved from the API
+            $url = $this->getDownloadUrl($url);
 
-			// Download file from different location
-			$originalRemoteFilesystem = $event->getRemoteFilesystem();
+            // Download file from different location
+            $originalRemoteFilesystem = $event->getRemoteFilesystem();
 
-			$event->setRemoteFilesystem(new RemoteFilesystem(
-				$url,
-				$this->io,
-				$this->composer->getConfig(),
-				$originalRemoteFilesystem->getOptions(),
-				$originalRemoteFilesystem->isTlsDisabled()
-			));
+            $event->setRemoteFilesystem(new RemoteFilesystem(
+                $url,
+                $this->io,
+                $this->composer->getConfig(),
+                $originalRemoteFilesystem->getOptions(),
+                $originalRemoteFilesystem->isTlsDisabled()
+            ));
 
-			// Stop other events from running and overriding this
-			$event->stopPropagation();
-		}
-	}
+            // Stop other events from running and overriding this
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * Get the AWS Download URL from the Gravity Forms API
-	 *
-	 * @param $url
-	 *
-	 * @return string
-	 * @throws DownloadException
-	 * @throws Exception
-	 */
+    /**
+     * Get the AWS Download URL from the Gravity Forms API
+     *
+     * @param $url
+     *
+     * @return string
+     * @throws DownloadException
+     * @throws Exception
+     */
     public function getDownloadUrl($url): string
     {
-		$result = file_get_contents($url, false, StreamContextFactory::getContext($url));
+        $result = file_get_contents($url, false, StreamContextFactory::getContext($url));
 
-		if(false === $result) {
-		    throw new DownloadException($url);
-		}
+        if (false === $result) {
+            throw new DownloadException($url);
+        }
 
-		$body        = trim($result);
-		$plugin_info = unserialize($body);
+        $body = trim($result);
+        $plugin_info = unserialize($body);
 
-		$download_url = isset($plugin_info['download_url_latest']) ? $plugin_info['download_url_latest'] : '';
+        $download_url = isset($plugin_info['download_url_latest']) ? $plugin_info['download_url_latest'] : '';
 
-		if(empty($download_url)) {
-		    throw new Exception( 'Unable to find download URL. Check your Gravity Forms API key.' );
-		}
+        if (empty($download_url)) {
+            throw new Exception('Unable to find download URL. Check your Gravity Forms API key.');
+        }
 
-		return (string) $download_url;
+        return (string) $download_url;
     }
 }
